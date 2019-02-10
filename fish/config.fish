508d1d4d530ffef589bdf2b54362fe1f2c7473d5
Kevin Leimkuhler <kevinl@buoyant.io>
2019-02-10 13:22:19 -0800

    Consolidate abbreviations

--- fish/config.fish
+++ fish/config.fish
@@ -5,28 +5,57 @@ and not set -q TMUX
 end
 
 # Abbreviations
-abbr -a e nvim
-abbr -a c cargo
-abbr -a co code
-abbr -a g git
+abbr -a -g co    code
+abbr -a -g e     nvim
+abbr -a -g mkdir 'mkdir -p'
+
+abbr -a -g c   cargo
+abbr -a -g cb  'cargo build'
+abbr -a -g cc  'cargo check'
+abbr -a -g ccl 'cargo clean'
+abbr -a -g cr  'cargo run'
+abbr -a -g ct  'cargo test'
+
+abbr -a -g g   git
+abbr -a -g ga  'git add'
+abbr -a -g gb  'git branch'
+abbr -a -g gc  'git commit'
+abbr -a -g gch 'git checkout'
+abbr -a -g gcl 'git clone'
+abbr -a -g gd  'git diff'
+abbr -a -g gf  'git fetch'
+abbr -a -g gl  'git log'
+abbr -a -g gm  'git merge'
+abbr -a -g gr  'git rebase'
+abbr -a -g grm 'git remote'
+abbr -a -g gpl 'git pull'
+abbr -a -g gps 'git push'
+abbr -a -g gss 'git stash'
+abbr -a -g gst 'git status'
+abbr -a -g gsm 'git submodule'
 
 if type -q exa
-    abbr -a l 'exa'
-    abbr -a la 'exa -al --git'
-    abbr -a ll 'exa -l --git'
-    abbr -a lt 'exa -T'
+    abbr -a -g l  'exa'
+    abbr -a -g la 'exa -al --git'
+    abbr -a -g ll 'exa -l --git'
+    abbr -a -g lt 'exa -T'
 else
-    abbr -a l 'ls'
-    abbr -a la 'ls -al'
-    abbr -a ll 'ls -l'
+    abbr -a -g l  'ls'
+    abbr -a -g la 'ls -al'
+    abbr -a -g ll 'ls -l'
 end
 
 # Additions to $fish_user_paths
 set -g fish_user_paths $HOME/.cargo/bin $fish_user_paths
+set -g fish_user_paths $HOME/Projects/go/bin $fish_user_paths
 
 # Environment variables
 if type -q bat
     set -x BAT_THEME "GitHub"
+end
+
+if type -q go
+    set -x GOPATH $HOME/Projects/go
 end
 
 if test -f /usr/local/share/autojump/autojump.fish
